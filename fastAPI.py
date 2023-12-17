fa345d94713f57b68813265824c6af94f95677d3
dangtrantrung <trung280584@gmail.com>
2023-12-17 17:29:26 +0700

    FastAPI - Auto Doc SwaggerUI

---
+++ fastAPI.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class MyItem(BaseModel):
+    name:str
+    price:float
+    ready:int=0
+
+app =FastAPI()
+
+@app.get("/")
+async def home():
+    return "Đây là home"
+
+@app.post("/submit")
+async def submit(item:MyItem):
+    print(item.name)
+    return "Save thành công: " + item.name
